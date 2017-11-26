14c4349c55e08386e156ee51f58116aa8e4ec406
neeraj1909 <neeraj1909@gmail.com>
2017-11-26 23:36:28 +0530

    imported loader from django.template

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 
 def index(request):
