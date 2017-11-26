2eff4bd225b64cc065892088f160e59862ef67e6
neeraj1909 <neeraj1909@gmail.com>
2017-11-26 23:42:30 +0530

    changed the index view. Used render method instead of loader

--- polls/views.py
+++ polls/views.py
@@ -3,16 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
