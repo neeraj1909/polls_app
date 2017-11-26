89e8572d11297ccaeb31abb900ac82b4a30f402d
neeraj1909 <neeraj1909@gmail.com>
2017-11-26 23:34:25 +0530

    imported Question from models

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
