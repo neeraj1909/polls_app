890a12e035fcc36a46f443cfc8ad05cb081e7c75
neeraj1909 <neeraj1909@gmail.com>
2017-11-27 23:58:38 +0530

    removed old index, detail, and results views and use Django's generic views instead

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 
 def index(request):
